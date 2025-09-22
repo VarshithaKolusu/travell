666b0ea7fae749f9df415e086999b79663eb7091
VarshithaKolusu <varshithakolusu2006@gmail.com>
2025-09-22 13:38:19 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/travell1/',
   plugins: [react()],
-  base: '/travell/' // Must match your repo name exactly
 });
